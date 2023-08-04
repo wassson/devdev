b44c5205950f53f9d16c494002e6d2ace4f0ac44
Austin Wasson <93844519+austinwasson@users.noreply.github.com>
2023-08-04 13:22:37 -0600

    devdev exec helper methods

--- lib/devdev.rb
+++ lib/devdev.rb
@@ -1,12 +1,22 @@
 module DevDev
   class << self
+    # view all commands
     def all
       cmd_hash
     end
+
+    # view next command
     def next; end
+
+    # skip command
+    def skip; end
+
+    # view command documentation
+    def info; end
 
     private
 
+    # this will be built from a config created by the user
     def cmd_hash
       @commands ||= { "brew install node": false, "yarn install": false, "rails db:create": false }
     end
